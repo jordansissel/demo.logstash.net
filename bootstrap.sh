28a0123b9bbcb28b9574ac607998545ca7cee178
Jordan Sissel <jls@semicomplete.com>
2012-10-11 23:44:51 -0700

    chef bootstrappery

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# From the 'ubuntu chef' docs
+# http://wiki.opscode.com/display/chef/Installing+Chef+Client+on+Ubuntu+or+Debian
+
+echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | tee /etc/apt/sources.list.d/opscode.list
+
+mkdir -p /etc/apt/trusted.gpg.d
+gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
+gpg --export packages@opscode.com | tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
+apt-get update
+
+apt-get install chef
