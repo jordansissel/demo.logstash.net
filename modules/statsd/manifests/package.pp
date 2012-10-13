3de696d538b8da94c708b5a3886eb85456ce1ee1
Jordan Sissel <jls@semicomplete.com>
2012-10-12 22:58:56 -0700

    Fix ordering

--- modules/statsd/manifests/package.pp
+++ modules/statsd/manifests/package.pp
@@ -1,9 +1,11 @@
 class statsd::package {
   user::app { "statsd": ; }
 
-  ::vcsrepo {
+  vcsrepo {
     "/app/statsd/statsd":
       ensure => latest,
+      require => User::App["statsd"],
+      owner => "statsd",
       provider => "git",
       source => "https://github.com/etsy/statsd.git"
   }
