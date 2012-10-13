99802ad0cd50b35a9e0c33cf1b75c0961b064e39
Jordan Sissel <jls@semicomplete.com>
2012-10-12 23:12:37 -0700

    Build the runit service and config

--- modules/statsd/manifests/service.pp
+++ modules/statsd/manifests/service.pp
@@ -1,6 +1,11 @@
 class statsd::service {
   include ::statsd::package
 
-  #runit::process {
-  #}
+  runit::process {
+    "statsd":
+      ensure => present,
+      require => Class["statsd::package"],
+      command => "node stats.js config.js",
+      directory => "/apps/statsd/statsd";
+  }
 }
