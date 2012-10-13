02382e1e0f59a18fbe6fc9b984ce47ab7e5c272e
Jordan Sissel <jls@semicomplete.com>
2012-10-13 00:14:55 -0700

    - get kibana going

--- modules/demosite/manifests/init.pp
+++ modules/demosite/manifests/init.pp
@@ -1,11 +1,23 @@
 class demosite {
   include apache
   include apache::params
+  include kibana
+
+  file {
+    "$apache::params::vdir/graphite.conf":
+      ensure => file,
+      source => "puppet:///modules/demosite/graphite.conf",
+      # I don't like notifying an external service (defined by the 'apache' class)
+      # but the puppetlabs/apache module defines it there and until I patch it
+      # this is what is required. After fixing the apache module, I'll be
+      # able to do: notify => Class["apache::service"]
+      notify => Service["httpd"];
+  }
 
   file {
     "$apache::params::vdir/demo.conf":
       ensure => file,
-      source => "puppet:///modules/demosite/demo.conf",
+      source => "puppet:///moddemo/demosite/demo.conf",
       # I don't like notifying an external service (defined by the 'apache' class)
       # but the puppetlabs/apache module defines it there and until I patch it
       # this is what is required. After fixing the apache module, I'll be
---
+++ modules/kibana/manifests/package.pp
@@ -0,0 +1,17 @@
+class kibana::package {
+  user::app { "kibana": ; }
+
+  package {
+    "ruby1.9.3": ensure => latest;
+    "libcurl4-openssl-dev": ensure => latest;
+  }
+
+  vcsrepo {
+    "/app/kibana/kibana":
+      ensure => latest,
+      require => User::App["statsd"],
+      owner => "kibana",
+      provider => "git",
+      source => "https://github.com/rashidkpc/Kibana"
+  }
+}
