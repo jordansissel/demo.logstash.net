10b770b50c4aeb22e26da8f788c82621c67ecda4
Jordan Sissel <jls@semicomplete.com>
2012-10-12 22:10:00 -0700

    - make runit::process ensurable

--- modules/runit/manifests/process.pp
+++ modules/runit/manifests/process.pp
@@ -1,6 +1,7 @@
 define runit::process(
   $command, # the command to run
   $user, # the user to run as
+  $ensure="present",
   $directory="/",
   $serviceroot="/etc/service",
   $logroot="/var/log/service"
@@ -11,23 +12,34 @@ define runit::process(
   $servicedir = "$serviceroot/$name"
   $logdir = "$logroot/$name"
 
+  $ensure_file = $ensure
+  $ensure_directory = $ensure ? { "absent": "absent", default => "present" }
+
+  if $ensure == "absent" {
+    exec {
+      "stop runit::process[$name]":
+        command => "/usr/bin/sv force-stop $servicedir",
+        before => File["$servicedir"];
+    }
+  }
+
   file {
     "$servicedir": 
-      ensure => directory;
+      ensure => $ensure_directory;
     "$servicedir/run":
-      ensure => file,
+      ensure => $ensure_file,
       require => [ File["$servicedir/log/run"], File["$logdir"] ],
       mode => 0755,
       content => template("runit/run.sh.erb");
     "$servicedir/log":
-      ensure => directory;
+      ensure => $ensure_file;
     "$servicedir/log/run":
       ensure => file,
       require => File["$logdir"],
       mode => 0755,
       content => template("runit/log.sh.erb");
     "$logdir":
-      ensure => directory,
+      ensure => $ensure_directory,
       require => User["runitlog"],
       owner => "runitlog";
   }
