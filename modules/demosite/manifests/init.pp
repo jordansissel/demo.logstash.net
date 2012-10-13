10bb17cc4dd7da6b3f3d33dd58ae71a21c38e87f
Jordan Sissel <jls@semicomplete.com>
2012-10-13 00:02:48 -0700

    Can't have graphite live anywhere other than the root path :\

--- modules/demosite/manifests/init.pp
+++ modules/demosite/manifests/init.pp
@@ -6,7 +6,11 @@ class demosite {
     "$apache::params::vdir/demo.conf":
       ensure => file,
       source => "puppet:///modules/demosite/demo.conf",
-      notify => Class["apache"];
+      # I don't like notifying an external service (defined by the 'apache' class)
+      # but the puppetlabs/apache module defines it there and until I patch it
+      # this is what is required. After fixing the apache module, I'll be
+      # able to do: notify => Class["apache::service"]
+      notify => Service["httpd"];
   }
     
 }
