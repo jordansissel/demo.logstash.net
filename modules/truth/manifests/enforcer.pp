3489e43e4624191d77ab9316c3034422a45fed0e
Jordan Sissel <jls@semicomplete.com>
2012-10-12 17:31:11 -0700

    Set default exec path

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -1,4 +1,8 @@
 class truth::enforcer {
+  Exec {
+    path => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin" ]
+  }
+
   include user::humans
   include virtualbox
 }
