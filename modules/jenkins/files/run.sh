bd50ace35a7f50994f4f67606b17aac384cbbe5a
Jordan Sissel <jls@semicomplete.com>
2012-10-14 00:50:13 -0700

    jenkins likes equal signs

--- modules/jenkins/files/run.sh
+++ modules/jenkins/files/run.sh
@@ -3,8 +3,8 @@
 [ ! -f jenkins.war ] && wget http://mirrors.jenkins-ci.org/war/1.485/jenkins.war
 
 exec java -jar jenkins.war \
-  --httpPort 8001 \
-  --httpListenAddress 127.0.0.1 \
-  --prefix jenkins \
-  --ajp13Port -1 \
-  --controlPort -1
+  --httpPort=8001 \
+  --httpListenAddress=127.0.0.1 \
+  --prefix=jenkins \
+  --ajp13Port=-1 \
+  --controlPort=-1
