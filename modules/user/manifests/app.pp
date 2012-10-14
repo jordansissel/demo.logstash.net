b8a2fe44dc7a9889a41ca54e6109884c03778fc7
Jordan Sissel <jls@semicomplete.com>
2012-10-14 00:34:29 -0700

    set app homes to /app/USER

--- modules/user/manifests/app.pp
+++ modules/user/manifests/app.pp
@@ -6,6 +6,7 @@ define user::app($ensure="present", $groups=["apps"], $shell="/bin/false") {
     "$name":
       ensure => $ensure,
       shell => $shell,
+      home => "/app/$name",
       groups => $groups;
   }
 
