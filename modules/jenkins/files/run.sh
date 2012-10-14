bb005e6e4f97c9b4301746a6ef328f8e5ab9ec6b
Jordan Sissel <jls@semicomplete.com>
2012-10-14 00:47:28 -0700

    Close up access

--- modules/jenkins/files/run.sh
+++ modules/jenkins/files/run.sh
@@ -2,4 +2,9 @@
 
 [ ! -f jenkins.war ] && wget http://mirrors.jenkins-ci.org/war/1.485/jenkins.war
 
-java -jar jenkins.war 
+exec java -jar jenkins.war \
+  --httpPort 8001 \
+  --httpListenAddress 127.0.0.1 \
+  --prefix jenkins \
+  --ajp13Port -1 \
+  --controlPort -1
