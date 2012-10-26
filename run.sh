02843e77581368c44ba005fb03c58762f6fee7cc
Jordan Sissel <jls@semicomplete.com>
2012-10-26 14:53:24 -0700

    - fuckin' lumberjack that shit, yo!

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ if ! which puppet > /dev/null 2>&1 ; then
   sh bootstrap.sh
 fi
 
-puppet apply --modulepath modules site.pp
+puppet apply --modulepath modules site.pp \
+  | /opt/lumberjack/bin/lumberjack.sh --ssl-ca-path ./lumberjack.crt --host localhost --port 5005 --field type=puppet -
