8cde5d8853eb9f537dd1aabd09e7795d0900aea4
Jordan Sissel <jls@semicomplete.com>
2012-11-02 14:27:37 -0700

    more work on an rvm installer

--- modules/rvm/manifests/install.pp
+++ modules/rvm/manifests/install.pp
@@ -1,23 +1,12 @@
-define rvm::install(
+define rvm::install (
   $user, # the user to install for
   $ensure="stable"
 ) {
-
-  $install_exec = "install rvm for $user"
-  $version_exec = "ensure rvm version for $user"
-
-  file {
-    "/usr/bin/bootstrap-rvm.bash":
-      ensure => file,
-      mode => 755,
-      owner => root,
-      group => root,
-      source => "puppet:///modules/rvm/bootstrap-rvm.bash";
-  }
+  include rvm::files
 
   exec {
     "get rvm for $user":
-      require => [User[$user], File["/usr/bin/bootstrap-rvm.bash"]],
+      require => [User[$user], Class["rvm::files"]],
       command => "/usr/bin/bootstrap-rvm.bash $branch",
       user => $user,
       path => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin" ];
