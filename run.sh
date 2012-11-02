7868e598fdabbf6ae00b80ef68f5c80113fc5e66
Jordan Sissel <jls@semicomplete.com>
2012-11-02 14:25:44 -0700

    tee as well

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ if ! which puppet > /dev/null 2>&1 ; then
   sh bootstrap.sh
 fi
 
-puppet apply --modulepath modules site.pp \
+puppet apply --color=false --modulepath modules site.pp \
+  | tee /dev/stderr \
   | /opt/lumberjack/bin/lumberjack.sh --ssl-ca-path ./lumberjack.crt \
        --host localhost --port 5005 --field type=puppet \
        --field run=`date +%s` -
