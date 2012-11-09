6451ed0cf4c233ebd1334bcd160ac1546b465f4f
Jordan Sissel <jls@semicomplete.com>
2012-11-08 21:54:59 -0800

    try and get bundle_exec working

--- modules/rvm/manifests/install.pp
+++ modules/rvm/manifests/install.pp
@@ -3,10 +3,11 @@ define rvm::install (
   $ensure="stable"
 ) {
   include rvm::files
+  include curl
 
   exec {
     "get rvm for $user":
-      require => [User[$user], Class["rvm::files"]],
+      require => [User[$user], Class["rvm::files"], Class["curl"]],
       command => "bash /usr/bin/bootstrap-rvm.bash $branch",
       user => $user,
       path => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin" ];
