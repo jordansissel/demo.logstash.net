6d5b3bbfdd1c66ae67df590b57ccca12001848bb
Jordan Sissel <jls@semicomplete.com>
2012-11-13 04:12:32 +0000

    set java.io.tmpdir to work around /tmp/ being noexec

--- modules/jenkins/files/run.sh
+++ modules/jenkins/files/run.sh
@@ -2,7 +2,8 @@
 
 [ ! -f jenkins.war ] && wget http://mirrors.jenkins-ci.org/war/1.485/jenkins.war
 
-exec java -jar jenkins.war \
+mkdir /app/jenkins/tmp
+exec java -Djava.io.tmpdir=/app/jenkins/tmp -jar jenkins.war \
   --httpPort=8001 \
   --httpListenAddress=127.0.0.1 \
   --prefix=/jenkins \
