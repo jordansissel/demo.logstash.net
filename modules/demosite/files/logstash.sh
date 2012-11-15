51d8f81cfe468e6ed38d856d0370a32d8853f9fd
Jordan Sissel <jls@semicomplete.com>
2012-11-15 08:45:00 +0000

    - generate an ssl cert for lumberjack

--- modules/demosite/files/logstash.sh
+++ modules/demosite/files/logstash.sh
@@ -7,4 +7,11 @@ if [ ! -f "$file" ] ; then
   wget -O "$file" "$url"
 fi
 
+if [ ! -f "server.crt" -o ! -f "server.key" ] ; then
+  rm -f server.{crt,key,csr}
+  openssl genrsa -out server.key 2048
+  openssl req -new -key server.key -batch -out server.csr
+  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+fi
+
 exec java -jar $file "$@"
