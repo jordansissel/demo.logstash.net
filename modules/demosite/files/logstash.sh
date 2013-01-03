0da924d534251d0921e13c76f3f34b9b2f983a88
Jordan Sissel <jls@semicomplete.com>
2013-01-02 17:39:15 -0800

    - use 1.1.6 and flatjar

--- modules/demosite/files/logstash.sh
+++ modules/demosite/files/logstash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-url="https://logstash.objects.dreamhost.com/release/logstash-1.1.5-monolithic.jar"
+url="https://logstash.objects.dreamhost.com/release/logstash-1.1.6-flatjar.jar"
 file=`basename "$url"`
 
 if [ ! -f "$file" ] ; then
