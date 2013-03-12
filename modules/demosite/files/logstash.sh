11eb6bfc2f2cef75e5bc24707efcb1b75b93459b
Jordan Sissel <jls@semicomplete.com>
2013-03-12 21:12:20 +0000

    - unpack geoip

--- modules/demosite/files/logstash.sh
+++ modules/demosite/files/logstash.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Yeah, I should package this, but whatever, it's a demo. ;)
 url="https://logstash.objects.dreamhost.com/release/logstash-1.1.9-monolithic.jar"
 file=`basename "$url"`
 
@@ -12,6 +13,11 @@ if [ ! -f "server.crt" -o ! -f "server.key" ] ; then
   openssl genrsa -out server.key 2048
   openssl req -new -key server.key -batch -out server.csr
   openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+fi
+
+if [ ! -f "GeoLite-City.dat" ] ; then
+  wget -qO - http://logstash.objects.dreamhost.com/maxmind/GeoLite-City-2013-01-18.dat.gz \
+    | gzip -dc > "GeoLite-City.dat"
 fi
 
 exec java -jar $file "$@"
