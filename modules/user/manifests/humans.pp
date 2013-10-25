431784de4ad3d8336ce26ff549b82c87943528c7
Jordan Sissel <jls@semicomplete.com>
2013-10-25 19:40:56 +0000

    Add electrical

--- modules/user/manifests/humans.pp
+++ modules/user/manifests/humans.pp
@@ -2,6 +2,7 @@ class user::humans {
   user::managed {
     "jls": ensure => present, root => true;
     "nethier": ensure => present, root => true;
+    "electrical": ensure => present, root => true;
   }
 }
 
