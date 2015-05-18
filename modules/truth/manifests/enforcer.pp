6191a3c1a8928eae67e1698b1395f0703afe419b
Jordan Sissel <jls@semicomplete.com>
2015-05-18 17:39:52 +0000

    Simplify puppet + add new 'latest' redirects

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -3,17 +3,17 @@ class truth::enforcer {
     path => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin" ]
   }
 
-  include user::humans
-  include os
+  #include user::humans
+  #include os
 
   if ($demo) {
-    include virtualbox
-    include statsd
-    include demosite
-    include demosite::logstash
-    include jenkins
-    include redis
-    include shatty
+    #include virtualbox
+    #include statsd
+    #include demosite
+    #include demosite::logstash
+    #include jenkins
+    #include redis
+    #include shatty
   }
   include logstashnet
 }
